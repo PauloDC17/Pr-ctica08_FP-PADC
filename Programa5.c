d092f4217c113161476420d7ac9535a0d8bfc29a
Paulo Calderon <pauloandresdc007@gmail.com>
2025-10-16 14:11:50 -0600

    Add program to calculate average grades

---
+++ Programa5.c
@@ -0,0 +1,20 @@
+// PAULO A DELGADO CALDERON XVI-X-MMXXV
+
+#include <stdio.h>
+int main ()
+{
+    int enteroNumAlumnos = 5;
+    float realCalif = 0.0, realPromedio = 0.0;
+    printf("\tPromedio de calificaciones\n");
+    for (int indice = 0 ; indice < enteroNumAlumnos ; indice++)
+    {
+        printf("\nIngrese la calificación del alumn %d\n", indice+1);
+        scanf("%f",&realCalif);
+        realPromedio += realCalif;
+    }
+    
+    printf("\nEl promedio de las calificaciones ingresadas es: %f\n",
+    realPromedio/enteroNumAlumnos);
+    
+    return 0;
+}
